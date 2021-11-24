3de2ccebe70bbed0fa050d40a61c5458140a2710
MateusWei <51674914+MateusWei@users.noreply.github.com>
2021-11-23 21:32:03 -0300

    Mudança de projeto

--- petshop/src/main/java/com/petshopweb/repository/AnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.petshopweb.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.petshopweb.models.Animal;
-
-public interface AnimalRepository extends CrudRepository<Animal, String> {
-
-}
